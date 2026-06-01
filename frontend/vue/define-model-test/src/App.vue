38f3c52dbe422716b5afc9b3bc0ab6690eccdb01
Talyra42 <talyra42@foxmail.com>
2026-06-01 11:50:15 +0800

    feat: 练习了 defineModel 的使用方式

---
+++ frontend/vue/define-model-test/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import MyDialog from "./components/MyDialog.vue";
+import { ref } from "vue";
+
+const showDialog = ref(false);
+</script>
+
+<template>
+  <h1>Hello DefineModel!</h1>
+  <p>A test for defineModel.</p>
+  <button @click="showDialog = true">Show Dialog</button>
+  <MyDialog v-model:show="showDialog"></MyDialog>
+</template>
---
+++ frontend/vue/define-model-test/src/components/MyDialog.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const showDialog = defineModel<boolean>("show", { required: true });
+</script>
+
+<template>
+  <Teleport to="body">
+    <Transition>
+      <div v-show="showDialog" class="dialog-container">
+        <h1>This is Title</h1>
+        <p>This is content</p>
+        <button @click="showDialog = false">Close Dialog</button>
+      </div>
+    </Transition>
+  </Teleport>
+</template>
+
+<style lang="css" scoped>
+.dialog-container {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  background-color: rgba(0, 0, 0, 0.192);
+  backdrop-filter: blur(5px);
+}
+</style>
