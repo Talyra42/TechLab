34edcae61c6b3a421907170fc8e9ae3634ade60c
Talyra42 <talyra42@foxmail.com>
2026-06-13 09:50:32 +0800

    feat: 实现了 Markdown 的完整渲染功能

--- backend/python/tui-basic/lesson_11.py
+++ backend/python/tui-basic/lesson_11.py
@@ -9,6 +9,28 @@ def write(s: str):
     sys.stdout.flush()
 
 
+def render_markdown(stream):
+    res = ""
+    buffer = ""
+    is_bold = 1
+
+    for chunk in stream:
+        for char in chunk:
+            if char == "*":
+                buffer += char
+            else:
+                if buffer == "*":
+                    res += buffer
+                    buffer = ""
+                res += char
+            if buffer == "**":
+                res += f"{ESC}[{str(is_bold)}m"
+                is_bold ^= 1
+                buffer = ""
+    res += f"{ESC}[0m"
+    return res
+
+
 def display_width(s: str) -> int:
     counter = 0
     in_escape = False
@@ -36,6 +58,10 @@ def wrap(text: str, max_width: int):
     res = ""
 
     for c in text:
+        if c == "\n":
+            res += buffer + "\n"
+            buffer = ""
+            continue
         if display_width(buffer + c) > max_width:
             res += buffer + "\n"
             buffer = c
@@ -46,5 +72,7 @@ def wrap(text: str, max_width: int):
 
 
 if __name__ == "__main__":
-    styled_text = f"普通文字{ESC}[1m加粗的部分{ESC}[0m后面还有很多文字内容测试换行"
-    print(wrap(styled_text, 20))
+    markdown = "Hello **Talyra42**! This is Good Job!\n**2 * 2 = 4**!"
+    styled = render_markdown(markdown)
+    wrapped = wrap(styled, 20)
+    write(wrapped + "\n")
