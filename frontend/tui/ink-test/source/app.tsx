e27de3c396e497bc8747a54e7702233051f5e710
Talyra42 <talyra42@foxmail.com>
2026-06-13 15:17:46 +0800

    feat: 学习了 Text 的基本使用

--- frontend/tui/ink-test/source/app.tsx
+++ frontend/tui/ink-test/source/app.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
-import {Text} from 'ink';
+import {Text, Box} from 'ink';
 
 type Props = {};
 
 export default function App({}: Props) {
-	return <Text color="green">My First Ink Program</Text>;
+  return (
+    <>
+      <Box width={7} flexDirection="column">
+        {/* 后面省略号 */}
+        <Text wrap="truncate-end">Hello World</Text>
+        {/* 前面省略号 */}
+        <Text wrap="truncate-start">Hello World</Text>
+        {/* 中间省略号 */}
+        <Text wrap="truncate-middle">Hello World</Text>
+      </Box>
+    </>
+  );
 }
